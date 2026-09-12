730ca85fa12484a2992b149960e699ba5a8c8d44
SiGMan <sigman@ioupg.com>
2026-09-11 22:20:01 -0700

    Dust tint and saturation controls

--- shaders/scene_shared.h
+++ shaders/scene_shared.h
@@ -30,7 +30,8 @@ struct Frame {
     SHADER_MATRIX4 belt_light_projection; // orthographic box over the whole belt from the sun
     SHADER_FLOAT4 belt_light;  // box half width, half height, map texels, half span of the slice range along the light
     SHADER_FLOAT4 quality;     // temporal anti-aliasing on, tone curve (0 ACES filmic, 1 AgX, 2 PBR Neutral), belt dust on, unused
-    SHADER_FLOAT4 dust;        // belt dust multipliers: density (extinction), brightness (albedo), far-view scale, unused
+    SHADER_FLOAT4 dust;        // belt dust multipliers: density (extinction), brightness (albedo), far-view scale, saturation
+    SHADER_FLOAT4 dust_tint;   // multiplies the dust colour, w unused
 };
 struct Root {
 #ifdef __cplusplus
