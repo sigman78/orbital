93ad17070e41a46bb20d09b5707274625ccc3750
SiGMan <sigman@ioupg.com>
2026-09-12 02:21:32 -0700

    Cloud shadows on the atmosphere below the cloud layer; cloud helpers shared in clouds.slang

--- cmake/Slang.cmake
+++ cmake/Slang.cmake
@@ -20,6 +20,7 @@ function(orbital_compile_slang output source entry stage)
                 "${CMAKE_SOURCE_DIR}/shaders/belt.slang"
                 "${CMAKE_SOURCE_DIR}/shaders/rockclass.slang"
                 "${CMAKE_SOURCE_DIR}/shaders/beltfar.slang"
+                "${CMAKE_SOURCE_DIR}/shaders/clouds.slang"
                 "${CMAKE_SOURCE_DIR}/shaders/scene_shared.h"
         VERBATIM
         COMMENT "Compiling Slang ${entry} (${stage})"
